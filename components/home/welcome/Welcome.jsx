10554d69460837e8475275d9eb5812ddcb4d31c4
R.L.D.T.H. Surasinghe <surasinghe_rldth_e23@engug.ruh.ac.lk>
2025-01-05 15:56:35 +0530

    refactor : update Welcome.jsx
    Add search bar
    Add job types

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -16,6 +16,7 @@ const jobTypes = ["Full-time", "Part-time", "Contractor"]
 
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full-time')
   
   return (
     <View>
@@ -43,14 +44,28 @@ const Welcome = () => {
             </TouchableOpacity>
           </View>
 
+
+
           <View style ={styles.tabsContainer}>
             <FlatList
               data = {jobTypes}
               renderItem = {({item}) => (
-            />
+                <TouchableOpacity
+                style= {styles.tab(activeJobType, item)}
+                onPress = {() =>{
+                  setActiveJobType(item);
+                  router.push(`/search/${item}`)
+                }} >
+                  <Text style = { styles.tabText(activeJobType, item )} >{item}</Text>
+                </TouchableOpacity>
+              )}
+              keyExtractor = {item => item}
+              contentContainerStyle = {{ columnGap : SIZES.small }}
+              horizontal
+            />           
           </View>
         </View>
-   )
+      )
 }
 
 export default Welcome
